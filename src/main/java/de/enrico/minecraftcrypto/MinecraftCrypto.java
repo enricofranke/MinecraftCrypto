654cc97967207dbfd386816b2ba9d50e16469ce7
T0myyde <lpmcbagi@gmail.com>
2021-02-08 21:58:58 +0100

    addet crypto command

--- src/main/java/de/enrico/minecraftcrypto/MinecraftCrypto.java
+++ src/main/java/de/enrico/minecraftcrypto/MinecraftCrypto.java
@@ -1,16 +1,21 @@
 package de.enrico.minecraftcrypto;
 
+import de.enrico.minecraftcrypto.commands.CryptoPrice;
 import org.bukkit.plugin.java.JavaPlugin;
 
 public class MinecraftCrypto extends JavaPlugin {
 
     @Override
     public void onEnable() {
-
+        onInit();
     }
 
     @Override
     public void onDisable() {
 
+    }
+
+    private void onInit() {
+        this.getCommand("crypto").setExecutor(new CryptoPrice());
     }
 }
--- src/main/java/de/enrico/minecraftcrypto/commands/CryptoPrice.java
+++ src/main/java/de/enrico/minecraftcrypto/commands/CryptoPrice.java
@@ -1,4 +1,36 @@
 package de.enrico.minecraftcrypto.commands;
 
-public class CryptoPrice {
+import org.bukkit.Bukkit;
+import org.bukkit.command.Command;
+import org.bukkit.command.CommandExecutor;
+import org.bukkit.command.CommandSender;
+import org.bukkit.entity.Player;
+
+public class CryptoPrice implements CommandExecutor {
+
+    public boolean onCommand(CommandSender sender, Command cmd, String label, String[] args) {
+        if (!(cmd instanceof Player)) {
+            Bukkit.getConsoleSender().sendMessage("§cERROR");
+            return false;
+        }
+        Player player = (Player)sender;
+
+        // /crypto <Crypto>
+        if (args.length == 1) {
+            switch (args[0]) {
+                case "Xrp":
+                    player.sendMessage("Xrp: ");
+                    break;
+                case "Btc":
+                    player.sendMessage("Btc: ");
+                    break;
+                default:
+                    break;
+            }
+        } else {
+            player.sendMessage("§cUse /crypto <Xrp/Btc>");
+        }
+            return true;
+    }
+
 }
